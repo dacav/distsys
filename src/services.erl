53b5df6f1354e9cf32b0b9332d7d0d491d07cf20
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2011-11-15 15:08:05 +0100

    Services supervisor.
    Supervising:
    - logging system;
    - random-delay channels.

---
+++ src/services.erl
@@ -0,0 +1,36 @@
+-module(services).
+-author("Giovanni Simoni").
+
+-behavior(supervisor).
+-export([init/1, start_link/1]).
+
+-import(channel).
+
+-define(TIMEOUT, 3000).
+-define(MAX_RESTART, 3).
+-define(MAX_TIME_RESTART, 1000).
+-define(KILL_THRESHOLD, 1000).
+
+-record(servconf, {chan, logger}).
+
+init (Params) ->
+    % Channel service descriptor
+    Channel = {channel,
+         {channel, start_link, Params#servconf.chan},
+         permanent, ?KILL_THRESHOLD, worker, [channel]
+    },
+    % Log service descriptor
+    Log = {logger,
+             {log_serv, start_link, Params#servconf.logger},
+             permanent, ?KILL_THRESHOLD, worker, [log_serv]
+    },
+    % GO!
+    {ok,
+    	{{one_for_one, ?MAX_RESTART, ?MAX_TIME_RESTART},
+         [Channel, Log]
+        }
+  	}.
+
+start_link (Params) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, Params).
+
