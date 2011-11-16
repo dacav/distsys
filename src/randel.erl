5696321ad71f89c2fef327e57b51a132cdbf3cf8
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2011-11-16 15:55:44 +0100

    Moved definition of random value specification.
    Outside randel.erl, in randel.hrl

--- src/randel.erl
+++ src/randel.erl
@@ -13,16 +13,7 @@
 -export([send_rand/3, send_rand/2, exit_rand/3, exit_rand/2, kill_rand/2,
          kill_rand/1]).
 
--define(DEFAULT_MIN, 0).
--define(DEFAULT_MAX, 1000).
--define(DEFAULT_DIST, fun random:uniform/0).
-
-% ------------------------------------------------------------------------
-% Random period specification:
-% ------------------------------------------------------------------------
--record(randspec, {min=?DEFAULT_MIN,
-                   max=?DEFAULT_MAX,
-                   dist=?DEFAULT_DIST}).
+-include("randel.hrl").
 
 random_time (Spec) ->
     Min = Spec#randspec.min,
