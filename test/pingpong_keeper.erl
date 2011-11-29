04c5b81ed709c1ffd8b119400e2cff6fc69d6bab
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2011-11-29 09:03:51 +0100

    Testing application update
    Ping pong works pretty well.

--- test/pingpong_keeper.erl
+++ test/pingpong_keeper.erl
@@ -2,22 +2,96 @@
 -author("Giovanni Simoni").
 -behavior(gen_keeper).
 -export([init/1, handle_spawn_notify/2, handle_result_notify/3,
-         handle_term_notify/3]).
+         handle_term_notify/4, handle_info/2]).
+
+-import(keeper_inject).
+-import(keeper_proto).
+
+-record(status, {waiting_for,
+                 spawned,
+                 npeers}).
 
 init (NPeers) ->
-    io:format("Hi there, this is pingpong_keeper initalizing...\n"),
-    Peers = keeper_proto:add_peers(NPeers, pingpong_peer, [nil]),
-    log_serv:log("Initialized: ~p", [Peers]),
-    {ok, Peers}.
+    PeersRef =
+        case keeper_proto:add_peers(NPeers, pingpong_peer, [nil]) of
+            {ok, Lst} -> Lst;
+            _ -> throw(no_spawn)
+        end,
+    Peers = lists:map(fun (T) -> element(1, T) end, PeersRef),
+    log_serv:log("Spawning ~p peers...\n> ~p", [NPeers, Peers]),
+    {ok, #status{waiting_for=NPeers, spawned=Peers, npeers=NPeers}}.
 
-handle_spawn_notify (Pid, Peers) ->
-    log_serv:log("Started: ~p", [Pid]),
-    {ok, Peers}.
+decrease_waiting (Status) ->
+    #status{waiting_for = Status#status.waiting_for - 1,
+            spawned = Status#status.spawned,
+            npeers = Status#status.npeers}.
 
-handle_result_notify (Pid, Result, Peers) ->
-    log_serv:log("Result from ~p: ~p~n", [Pid, Result]),
-    {ok, Peers}.
+kill_and_remove (Pid, Status) ->
+    exit(Pid, kill),
+    Spawned = lists:filter(fun (X) -> X =:= Pid end,
+                           Status#status.spawned),
+    case Spawned of
+        [] -> stop;
+        _ -> #status{waiting_for = Status#status.waiting_for,
+                     spawned = Spawned,
+                     npeers = Status#status.npeers - 1}
+    end.
 
-handle_term_notify (Pid, Reason, Peers) ->
+meet ([], Y) ->
+    erlang:error(assertion, [[], Y]);
+meet ([X], First) ->
+    keeper_inject:introduce(X, First);
+meet ([X, Y | Others], First) ->
+    keeper_inject:introduce(X, Y),
+    meet([Y | Others], First).
+
+meet (All = [Fst | _]) ->
+    meet(All, Fst).
+
+run_pingpong(Spawned, N) ->
+    % Let them meet each other!
+    meet(Spawned),
+    % Random selection of a starting pid
+    SelPid = lists:nth(random:uniform(N), Spawned),
+    log_serv:log("Injecting on ~p", [SelPid]),
+    % Go!
+    Homework = 3 * N,
+    keeper_inject:send(SelPid, Homework),
+    ok.
+
+handle_spawn_notify (Pid, #status{waiting_for=0}) ->
+    log_serv:log("Peer ~p seems to be spawned twice!", [Pid]),
+    {error, phantom_peer};
+
+handle_spawn_notify (Pid, Status) ->
+    case Status#status.waiting_for of
+        1 ->
+            log_serv:log("Confirm by ~p. Last ok, ready to go!", [Pid]),
+            run_pingpong(Status#status.spawned,
+                         Status#status.npeers);
+        N ->
+            log_serv:log("Confirm by ~p. Waiting for ~p nodes...",
+                         [Pid, N])
+    end,
+    {ok, decrease_waiting(Status)}.
+
+handle_result_notify (Pid, Result, Status) ->
+    log_serv:log("Result from ~p: ~p", [Pid, Result]),
+    case kill_and_remove(Pid, Status) of
+        stop ->
+            log_serv:log("Game over."),
+            stop;
+        NewStatus ->
+            log_serv:log("Killed the process. Now restart!"),
+            run_pingpong(Status#status.spawned,
+                         Status#status.npeers),
+            {ok, NewStatus}
+    end.
+
+handle_term_notify (Pid, _Ref, Reason, Status) ->
     log_serv:log("Terminated ~p: ~p~n", [Pid, Reason]),
-    {ok, Peers}.
+    {ok, Status}.
+
+handle_info (Info, Status) ->
+    log_serv:log("Got info: ~p", [Info]),
+    {ok, Status}.
--- test/pingpong_peer.erl
+++ test/pingpong_peer.erl
@@ -1,18 +1,34 @@
 -module(pingpong_peer).
 -author("Giovanni Simoni").
 -behavior(gen_peer).
--export([init/1, handle_message/3, handle_introduction/3,
-         handle_noise/2]).
+-export([init/1, handle_message/3, handle_introduction/3, handle_info/2]).
+
+-import(peer_chan).
+-import(peer_ctrl).
 
 init (Arg) ->
-    io:format("I'm alive! (~p, Arg=~p)", [self(), Arg]),
+    log_serv:log("I'm alive! (~p, Arg=~p)", [self(), Arg]),
+    peer_ctrl:notify_spawn(),
     {ok, nil}.
 
-handle_message (_From, _To, _Prvt) ->
-    {ok, nil}.
+handle_message (From, N, Next) when is_number(N) ->
+    log_serv:log("Homework from ~p: N=~p", [From, N]),
+    case N of
+        0 -> peer_ctrl:notify_result(finish);
+        _ -> peer_chan:send(Next, N - 1)
+    end,
+    {ok, Next};
 
-handle_introduction (_From, _NewNode, _Prvt) ->
-    {ok, nil}.
+handle_message (From, Msg, Next) ->
+    log_serv:log("Message from ~p saying ~p", [From, Msg]),
+    {ok, Next}.
 
-handle_noise (_Noise, _Prvt) ->
-    {ok, nil}.
+handle_introduction (From, NewNode, Next) ->
+    log_serv:log("Introduction from ~p. Setting next: from ~p to ~p",
+                 [From, Next, NewNode]),
+    peer_chan:send(NewNode, "Oh hi!"),
+    {ok, NewNode}.
+
+handle_info (Noise, Next) ->
+    log_serv:log("Got noise: ~p", [Noise]),
+    {ok, Next}.
