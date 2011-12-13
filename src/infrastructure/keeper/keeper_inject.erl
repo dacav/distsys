c70696aa806b01a94f0a10e03f30b84455873137
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2011-12-13 23:59:12 +0100

    Bugfix
    Squeezed this badass bug in the protocol! The broadcast was buggy!

--- src/infrastructure/keeper/keeper_inject.erl
+++ src/infrastructure/keeper/keeper_inject.erl
@@ -17,7 +17,7 @@ introduce (SpoofFrom, To, OtherNode) ->
     tweaked_chan:untweaked_send(SpoofFrom, To, {meet, OtherNode}).
 
 bcast (SpoofFrom, Msg) ->
-    tweaked_chan:untweaked_send(SpoofFrom, bcast, Msg).
+    tweaked_chan:untweaked_send(SpoofFrom, bcast, {mail, Msg}).
 
 bcast (Msg) ->
-    tweaked_chan:untweaked_send(keeper, bcast, Msg).
+    tweaked_chan:untweaked_send(keeper, bcast, {mail, Msg}).
